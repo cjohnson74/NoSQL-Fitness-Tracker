b2f80d906ed51d164b89c61d4635e062c1089539
Carson Johnson <carson74johnson@gmail.com>
2021-11-18 17:43:43 -0500

    added a find and update workout

--- controllers/api.js
+++ controllers/api.js
@@ -5,13 +5,22 @@ router.post('/workouts', (req, res) => {
     // add code here
     Workout.find({})
     .then(dbWorkout => {
-        res.json(dbWorkout);
-    });
+        res.status(200).json(dbWorkout);
+    })
+    .cantch((err) => {
+        res.status(500).json(err);
+    })
 });
 
 router.put('/workouts/:id', ({ body, params }, res) => {
     // add code here
     Workout.findOneAndUpdate(params.id, body)
+    .then(dbWorkout => {
+        res.status(200).json(dbWorkout, "Workout successfully updated.")
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    })
 });
 
 
