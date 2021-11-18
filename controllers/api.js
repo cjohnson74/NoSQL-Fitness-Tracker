52845ff79ec686b215df1cc6a6fd540ed3c3283b
Carson Johnson <carson74johnson@gmail.com>
2021-11-18 17:50:25 -0500

    added json res in workout post route

--- controllers/api.js
+++ controllers/api.js
@@ -4,28 +4,31 @@ const Workout = require('../models/workout.js');
 router.get('/workouts', (req, res) => {
     // find all workouts in the db
     Workout.find({})
-    .then(dbWorkout => {
+    .then((dbWorkout) => {
         res.status(200).json(dbWorkout);
     })
     .cantch((err) => {
         res.status(500).json(err);
-    })
+    });
 });
 
 router.put('/workouts/:id', ({ body, params }, res) => {
     // find a workout in the db and update it
     Workout.findOneAndUpdate(params.id, body)
-    .then(dbWorkout => {
+    .then((dbWorkout) => {
         res.status(200).json(dbWorkout, "Workout successfully updated.")
     })
     .catch((err) => {
         res.status(500).json(err);
-    })
+    });
 });
 
 router.post('/workouts', ({ body }, res) => {
     // create a workout and add it to the db
     Workout.create(body)
+    .then((dbWorkout) => {
+        res.status(200).json(dbWorkout);
+    })
 });
 
 module.exports = router;
