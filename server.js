a5da69d9fbf9d9f67e2e198c35ad49f78959a73d
Carson Johnson <carson74johnson@gmail.com>
2021-11-24 23:07:55 -0500

    fixed mongoose connect configs

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
